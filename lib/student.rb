56613f973b03fbfd02b43d4b6f375d58305e46b0
Martin Moy <martinmoy1991@gmail.com>
2020-03-20 08:10:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,5 +72,16 @@ class Student
     end
   end
 
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  end
 
 end
