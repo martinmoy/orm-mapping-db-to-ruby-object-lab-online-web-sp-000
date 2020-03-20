3cacb788f83f0254a7c41f90b2ec2ee33623fc87
Martin Moy <martinmoy1991@gmail.com>
2020-03-20 08:04:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,14 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
 
   end
 
